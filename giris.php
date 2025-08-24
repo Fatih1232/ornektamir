624bc1b77d2f0af9effbcc81f48748472790b717
Fatih1232 <gscimbom0639@gmail.com>
2025-08-25 01:39:09 +0300

    Update giris.php

--- giris.php
+++ giris.php
@@ -1,19 +1,20 @@
 <?php
 session_start();
 
+// Örnek kullanıcı adı ve şifre
 $dogru_kullanici = "behzat06";
 $dogru_sifre = "B0650";
 
-$kullanici_adi = $_POST['kullanici_adi'];
+$kullanici = $_POST['kullanici_adi'];
 $sifre = $_POST['sifre'];
 
-if ($kullanici_adi === $dogru_kullanici && $sifre === $dogru_sifre) {
-    $_SESSION['giris'] = true; // Kullanıcı oturumu açıldı
-    header("Location: giris.php");
-    exit;
-} else {
-    echo "<h3>❌ Kullanıcı adı veya şifre yanlış!</h3>";
-    echo "<a href='index.html'>Tekrar dene</a>";
-
+if($kullanici == $dogru_kullanici && $sifre == $dogru_sifre){
+    $_SESSION['giris'] = true; // Oturum açıldı
+    header("Location: index.html"); // Giriş başarılıysa index.html'e yönlendir
+    exit();
+}else{
+    echo "Kullanıcı adı veya şifre yanlış!";
 }
+?>
+
 
