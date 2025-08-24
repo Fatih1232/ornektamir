b68893f9e6d4210eed1985097fee7d968d838812
Fatih1232 <gscimbom0639@gmail.com>
2025-08-25 00:30:51 +0300

    Update giris.php

--- giris.php
+++ giris.php
@@ -9,10 +9,11 @@ $sifre = $_POST['sifre'];
 
 if ($kullanici_adi === $dogru_kullanici && $sifre === $dogru_sifre) {
     $_SESSION['giris'] = true; // Kullanıcı oturumu açıldı
-    header("Location: index.html");
+    header("Location: giris.php");
     exit;
 } else {
     echo "<h3>❌ Kullanıcı adı veya şifre yanlış!</h3>";
     echo "<a href='index.html'>Tekrar dene</a>";
 
 }
+
